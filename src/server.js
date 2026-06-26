5e301ddf161d94f11875da2826747ddc700f88a6
Andrii <andreymit123@gmail.com>
2026-06-26 15:12:58 +0300

    add User Registration

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello, World!' });
 });
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 // 404 — якщо маршрут не знайдено
